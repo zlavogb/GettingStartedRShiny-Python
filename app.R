0cf20a111782e2095e1a162b5ede547a9fba940d
zlavogb <zlavogb@uw.edu>
2020-05-18 16:19:12 -0700

    update for movie recommender system

--- app.R
+++ app.R
@@ -1,69 +1,67 @@
-# Define UI for app that draws a histogram ----
-library(reticulate)
 library(shiny)
+library(data.table)
+library(reticulate)
 
+imdb_data = fread("imdb_df_subset.csv")
+
+# Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  # App title ----
-  titlePanel("Hello Shiny!"),
+  # Application title
+  titlePanel("Movie selector"),
+ 
   
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot"),
-      textOutput("textOutput")
-      
+  fluidRow(
+    tabsetPanel(
+      type = "tabs",
+      # summary tab
+      tabPanel("Select Genre", uiOutput("genres"), htmlOutput("one")),
+      tabPanel("Select Year", uiOutput("years"), htmlOutput("two"))
     )
   )
 )
 
 source_python('python_ref.py')
 
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-    
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
+  output$genres<-renderUI({
+    selectInput("genre", label = h4("Choose genre"),
+                choices = sort(unique(imdb_data$genres)),selected = 1
+    )
   })
   
-  output$textOutput <- renderText({
+  output$years<-renderUI({
+    selectInput("yr", label = h4("Choose year"), 
+                choices = sort(unique(imdb_data$startYear)),
+                multiple = F)
+  })
+  
+  output$one <- renderUI({
     
-    value = input$bins
+    value = input$genre
+    returnedText = matchGenre(value)
+    ls=""
+    for(i in returnedText){
+      ls = HTML(paste(ls, i, sep="<br/>"))
+    }
+    print(ls)
+  })
+  
+  output$two <- renderUI({
     
-    returnedText = testMethod(value)
+    value2 = input$yr
+    returnedText2 = matchYear(as.integer(value2))
+    ls2=""
+    for(i in returnedText2){
+      ls2 = HTML(paste(ls2, i, sep="<br/>"))
+    }
+    print(ls2)
   })
   
 }
 
-shinyApp(ui, server)
+# Run the application 
+shinyApp(ui = ui, server = server)
+
--- python_ref.py
+++ python_ref.py
@@ -1,4 +1,20 @@
-def testMethod(bins):
-  string = "I came from a Python Function"
-  noOfBins = str(bins)
-  return "You have selected " +noOfBins+ " bins and " +string
+import pandas as pd
+
+
+def matchGenre(genre:str):
+  imdb_dt = pd.read_csv('imdb_df_subset.csv')
+  imdb_dt = imdb_dt[imdb_dt['genres']==genre]
+  imdb_dt = imdb_dt.sort_values(by='weightedAverage', ascending=False)
+  movielist = imdb_dt.primaryTitle.to_numpy()
+  if len(movielist)>10:
+    movielist = movielist[0:10]
+  return movielist
+
+def matchYear(yr:int):
+  imdb_dt = pd.read_csv('imdb_df_subset.csv')
+  imdb_dt = imdb_dt[imdb_dt['startYear']==yr]
+  imdb_dt = imdb_dt.sort_values(by='weightedAverage', ascending=False)
+  movielist2 = imdb_dt.primaryTitle.to_numpy()
+  if len(movielist2)>10:
+    movielist2 = movielist2[0:10]
+  return movielist2
